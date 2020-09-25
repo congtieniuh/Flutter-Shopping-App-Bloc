a6085f34fb288c71408f16742d8baf3584d4067f
congtieniuh <congtien.iuh@gmail.com>
2020-09-25 20:37:06 +0700

    add. Add common resources.

---
+++ sneaker_app/lib/common/app_theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+final ThemeData themeData = new ThemeData(
+    brightness: Brightness.light,
+    primarySwatch: MaterialColor(AppColors.orange[500].value, AppColors.orange),
+    primaryColor: AppColors.orange[500],
+    primaryColorBrightness: Brightness.light,
+    accentColor: AppColors.orange[500],
+    accentColorBrightness: Brightness.light
+);
+
+final ThemeData themeDataDark = ThemeData(
+  brightness: Brightness.dark,
+  primaryColor: AppColors.orange[500],
+  primaryColorBrightness: Brightness.dark,
+  accentColor: AppColors.orange[500],
+  accentColorBrightness: Brightness.dark,
+);
